1946c3e9788ba7a1a6aa4bd2950337e74e787472
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2022-03-16 13:58:42 +0100

    title

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
-
-    const loading = true
-
-    if (loading) return <h1>Loading...</h1>
-    
-    
     return (
         <div className="'container">
-        <h1>{title.toUpperCase()}</h1>
-        <p>{body}</p> 
-
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                     {comments.map((comments, index) => (
-                         <li key={index}>{comments.text}</li>
-                     ))}
-                </ul>
-            </div>
+        <h1>My App</h1>
         </div>
     )
 }
